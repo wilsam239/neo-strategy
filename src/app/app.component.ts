ac564a3333d4b168eefc821807d6b279aaa3d836
wilsam239 <swilliamson239@hotmail.com>
2022-07-04 22:06:53 +1000

    Project init stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from "@angular/core";
+import { Observable, of, BehaviorSubject, fromEvent, map, scan, pairwise, combineLatest, startWith } from "rxjs";
 
+import * as Konva from 'konva';
+
+interface PriorityItem {
+    title: string;
+    icon?: string;
+}
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'neo-strategy';
+  title = "neo-strategy";
+
+  priorities: PriorityItem[] = [];
+
+  @ViewChild("matrix", { static: false })
+  myCanvas: ElementRef<HTMLCanvasElement> = {} as ElementRef<any>;
+
+  matrixContext!: CanvasRenderingContext2D | null;
+
+  addListItem() {
+    this.priorities.push({title: 'item1'})
+  }
+
+  removeListItem(i: number) {
+    this.priorities.splice(i, 1);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import Konva from 'konva';
+
+const MATERIAL_LIBS = [
+    MatCardModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatIconModule
+    
+]
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    ...MATERIAL_LIBS,
   ],
   providers: [],
   bootstrap: [AppComponent]

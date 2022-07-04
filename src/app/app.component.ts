ba5bbf82d61adead3e2b5adc79d8835237d2d9dc
wilsam239 <swilliamson239@hotmail.com>
2022-07-04 22:17:29 +1000

    Konva test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component, ElementRef, ViewChild } from "@angular/core";
 import { Observable, of, BehaviorSubject, fromEvent, map, scan, pairwise, combineLatest, startWith } from "rxjs";
-
-import * as Konva from 'konva';
+import { Stage } from 'konva/lib/Stage';
+import { Layer } from 'konva/lib/Layer';
+import { Circle } from 'konva/lib/shapes/Circle';
 
 interface PriorityItem {
     title: string;
@@ -30,4 +31,39 @@ export class AppComponent {
     this.priorities.splice(i, 1);
   }
 
+  konvaTest() {
+    // first we need to create a stage
+var stage = new Stage({
+    container: 'matrix',   // id of container <div>
+    width: 500,
+    height: 500
+  });
+  
+  // then create layer
+  var layer = new Layer();
+  
+  // create our shape
+  var circle = new Circle({
+    x: stage.width() / 2,
+    y: stage.height() / 2,
+    radius: 70,
+    fill: 'red',
+    stroke: 'black',
+    strokeWidth: 4,
+    draggable: true
+  });
+
+  circle.on('dragend', (e) => {
+    console.log(e)
+  })
+  
+  // add the shape to the layer
+  layer.add(circle);
+  
+  // add the layer to the stage
+  stage.add(layer);
+  
+  // draw the image
+  layer.draw();
+  }
 }
